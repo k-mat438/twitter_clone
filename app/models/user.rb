dbe0039b965a9b05040cbd4813a29c8c8ae94c8f
k-mat438 <a1sco00ocs1a@gmail.com>
2024-02-10 11:09:13 +0900

    update model/user view/session

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  # validates :name, presence: true,
+  # validates :email, presence: true,
+  validates :name, presence: true, length: {maximum: 10}
+  validates :phone_number, presence: true, uniqueness: true
+  validates :birth, presence: true
+  # validates :password, presence: true,
 end
