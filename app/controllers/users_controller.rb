842c07aa6c22377aacba0995415421674fdb9c09
k-mat438 <a1sco00ocs1a@gmail.com>
2024-03-05 14:15:00 +0900

    rubocop実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,15 @@
 
 class UsersController < ApplicationController
   before_action :set_target_user, only: %i[show show_reposts show_comments show_favorites edit update]
-  def show
-  end
+  def show; end
 
-  def show_reposts
-  end
+  def show_reposts; end
 
   def show_comments; end
 
   def show_favorites; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @user.update(params_user)
@@ -21,8 +18,9 @@ class UsersController < ApplicationController
   end
 
   private
+
   def params_user
-    params.require(:user).permit(:name, :image, :header_image, :location, :website, :birth, :introduction,)
+    params.require(:user).permit(:name, :image, :header_image, :location, :website, :birth, :introduction)
   end
 
   def set_target_user
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   get 'users/show', to: 'users#show'
-  get 'users/show_reposts', to:'users#show_reposts'
-  get 'users/show_comments', to:'users#show_comments'
+  get 'users/show_reposts', to: 'users#show_reposts'
+  get 'users/show_comments', to: 'users#show_comments'
   get 'users/show_favorites', to: 'users#show_favorites'
   get 'users/profile_edit', to: 'users#edit'
   patch 'users/profile_edit', to: 'users#update'
