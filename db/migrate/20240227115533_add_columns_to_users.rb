dd070b8b90aeaf6637b8978ffe11df7f78ec0e0f
k-mat438 <a1sco00ocs1a@gmail.com>
2024-03-05 14:32:09 +0900

    bulkchangeテーブルに変更

--- db/migrate/20240227115533_add_columns_to_users.rb
+++ db/migrate/20240227115533_add_columns_to_users.rb
@@ -2,8 +2,10 @@
 
 class AddColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :introduction, :string, default: ''
-    add_column :users, :location, :string, default: '地球'
-    add_column :users, :website, :string, default: ''
+    change_table :users, bulk: true do |t|
+      t.string :introduction, default: ''
+      t.string :location, default: '地球'
+      t.string :website, default: ''
+    end
   end
 end
