18381065bfa2bd7424d7072d330140f8ef80de46
k-mat438 <a1sco00ocs1a@gmail.com>
2024-03-07 22:24:31 +0900

    conflict修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,8 @@
 
 class UsersController < ApplicationController
   before_action :set_target_user, only: %i[show show_reposts show_comments show_favorites edit]
-  def show
-    # @posts = Post.where(user_id: @user.id).includes(:user).order('created_at DESC')
-  end
+  def show; end
+  # @posts = Post.where(user_id: @user.id).includes(:user).order('created_at DESC')
 
   def show_reposts; end
 
