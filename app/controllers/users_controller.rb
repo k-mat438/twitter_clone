1a6e5325789683d99f25f20e0dc272e5896cba78
k-mat438 <a1sco00ocs1a@gmail.com>
2024-03-17 15:37:10 +0900

    rubocop訂正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_target_user, only: %i[show reposts comments favorites edit]
+  before_action :set_target_user, only: %i[show edit]
   def show
     @kind = params[:kind]
     case params[:kind]
